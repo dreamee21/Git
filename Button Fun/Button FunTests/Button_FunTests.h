33f6e0c6996a8a73b791b121fb6e670479a1abf6
Chris Kim <dreamee21@gmail.com>
2013-03-13 23:36:34 +0900

    Button Fun Create

---
+++ Button Fun/Button FunTests/Button_FunTests.h
@@ -0,0 +1,13 @@
+//
+//  Button_FunTests.h
+//  Button FunTests
+//
+//  Created by Chris Kim on 13. 3. 13..
+//  Copyright (c) 2013년 Chris Kim. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Button_FunTests : SenTestCase
+
+@end
---
+++ Button Fun/Button FunTests/Button_FunTests.m
@@ -0,0 +1,32 @@
+//
+//  Button_FunTests.m
+//  Button FunTests
+//
+//  Created by Chris Kim on 13. 3. 13..
+//  Copyright (c) 2013년 Chris Kim. All rights reserved.
+//
+
+#import "Button_FunTests.h"
+
+@implementation Button_FunTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Button FunTests");
+}
+
+@end
