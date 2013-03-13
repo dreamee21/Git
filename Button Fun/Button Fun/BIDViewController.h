4087f1286b7954c5cc8458bf0f8afc775d0b61ec
Chris Kim <dreamee21@gmail.com>
2013-03-13 23:36:34 +0900

    Button Fun Create

---
+++ Button Fun/Button Fun/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Button Fun
+//
+//  Created by Chris Kim on 13. 3. 13..
+//  Copyright (c) 2013년 Chris Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
---
+++ Button Fun/Button Fun/BIDViewController.m
@@ -0,0 +1,29 @@
+//
+//  BIDViewController.m
+//  Button Fun
+//
+//  Created by Chris Kim on 13. 3. 13..
+//  Copyright (c) 2013년 Chris Kim. All rights reserved.
+//
+
+#import "BIDViewController.h"
+
+@interface BIDViewController ()
+
+@end
+
+@implementation BIDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
