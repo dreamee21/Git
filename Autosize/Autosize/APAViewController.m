aa09513df3737c7598265a4ede5bae649c61da4f
Chris <dreamee21@gmail.com>
2013-03-27 02:35:24 +0900

    Autosize Create

---
+++ Autosize/Autosize/APAViewController.m
@@ -0,0 +1,29 @@
+//
+//  APAViewController.m
+//  Autosize
+//
+//  Created by Chris Kim on 13. 3. 27..
+//  Copyright (c) 2013년 Chris Kim. All rights reserved.
+//
+
+#import "APAViewController.h"
+
+@interface APAViewController ()
+
+@end
+
+@implementation APAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
