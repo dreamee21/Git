2f4122e5253bf8bc5baa7fba19856ff46ecdb091
Chris <dreamee21@gmail.com>
2013-03-29 02:11:39 +0900

    Revert "View Switcher 구현 2"
    This reverts commit 8a0c50070115fb1c14e0eabff15612a5cfd561bc.

--- View Switcher/BIDSwitchViewController.h
+++ View Switcher/BIDSwitchViewController.h
@@ -8,14 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@class BIDYellowViewController;
-@class BIDBlueViewController;
-
 @interface BIDSwitchViewController : UIViewController
-
-@property (strong, nonatomic) BIDYellowViewController *yellowViewController;
-@property (strong, nonatomic) BIDBlueViewController *blueViewController;
-
--(IBAction)switchViews:(id)sender;
 
 @end
--- View Switcher/BIDSwitchViewController.m
+++ View Switcher/BIDSwitchViewController.m
@@ -7,16 +7,12 @@
 //
 
 #import "BIDSwitchViewController.h"
-#import "BIDYellowViewController.h"
-#import "BIDBlueViewController.h"
 
 @interface BIDSwitchViewController ()
 
 @end
 
 @implementation BIDSwitchViewController
-@synthesize yellowViewController;
-@synthesize blueViewController;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,8 +25,6 @@
 
 - (void)viewDidLoad
 {
-    self.blueViewController = [[BIDBlueViewController alloc] initWithNibName:@"BlueView" bundle:nil];
-    [self.view insertSubview:self.blueViewController atIndex:0];
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
 }
@@ -39,27 +33,6 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-    
-    if (self.blueViewController.view.superview == nil) {
-        self.blueViewController = nil;
-    } else {
-        self.yellowViewController = nil;
-    }
 }
 
--(IBAction)switchViews:(id)sender{
-    if (self.yellowViewController.view.superview == nil) {
-        if(self.yellowViewController ==nil) {
-           self.yellowViewController = [[BIDYellowViewController alloc] initWithNibName:@"YellowView" bundle:nil];
-        }
-        [blueViewController.view removeFromSuperview];
-        [self.view insertSubview:self.yellowViewController.view atIndex:0];
-    } else {
-        if (self.blueViewController == nil) {
-            self.blueViewController = [[BIDBlueViewController alloc] initWithNibName:@"BlueView" bundle:nil];
-        }
-        [yellowViewController.view removeFromSuperview];
-        [self.view insertSubview:blueViewController.view atIndex:0];
-    }
-}
 @end
