f8693563decb0086f361add2aa927e7d4a1d8f8c
Chris Kim <earth@aurumplanet.com>
2013-03-30 00:29:52 +0900

    Revert "Pickers Delete"
    This reverts commit fa96c44d911b2db2e153f8e8d9fb1d9d6f6a72cd.

---
+++ Pickers/Pickers/BIDAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  BIDAppDelegate.h
+//  Pickers
+//
+//  Created by Chris Kim on 13. 3. 29..
+//  Copyright (c) 2013년 Chris Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) IBOutlet UITabBarController *rootController;
+
+@end
---
+++ Pickers/PickersTests/PickersTests.m
@@ -0,0 +1,32 @@
+//
+//  PickersTests.m
+//  PickersTests
+//
+//  Created by Chris Kim on 13. 3. 29..
+//  Copyright (c) 2013년 Chris Kim. All rights reserved.
+//
+
+#import "PickersTests.h"
+
+@implementation PickersTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PickersTests");
+}
+
+@end
