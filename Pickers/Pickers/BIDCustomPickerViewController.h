aec85921607956168339e0cb378855013dedc06d
Chris Kim <earth@aurumplanet.com>
2013-04-03 17:06:08 +0900

    Pickers Incomplete

--- Pickers/Pickers/BIDCustomPickerViewController.h
+++ Pickers/Pickers/BIDCustomPickerViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface BIDCustomPickerViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIPickerView *picker;
+@property (strong, nonatomic) IBOutlet UILabel *winLabel;
+@property (strong, nonatomic) NSArray *column1;
+@property (strong, nonatomic) NSArray *column2;
+@property (strong, nonatomic) NSArray *column3;
+@property (strong, nonatomic) NSArray *column4;
+@property (strong, nonatomic) NSArray *column5;
 
+- (IBAction)spin;
 @end
--- Pickers/Pickers/BIDCustomPickerViewController.m
+++ Pickers/Pickers/BIDCustomPickerViewController.m
@@ -13,6 +13,13 @@
 @end
 
 @implementation BIDCustomPickerViewController
+@synthesize picker;
+@synthesize winLabel;
+@synthesize column1;
+@synthesize column2;
+@synthesize column3;
+@synthesize column4;
+@synthesize column5;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,12 +34,73 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    UIImage *seven = [UIImage imageNamed:@"seven.png"];
+    UIImage *bar = [UIImage imageNamed:@"bar.png"];
+    UIImage *crown = [UIImage imageNamed:@"crown.png"];
+    UIImage *cherry = [UIImage imageNamed:@"cherry.png"];
+    UIImage *lemon = [UIImage imageNamed:@"lemon.png"];
+    UIImage *apple = [UIImage imageNamed:@"apple.png"];
+    
+    for (int i = 1; i <= 5; i++) {
+        UIImageView *sevenView = [[UIImageView alloc] initWithImage:seven];
+        UIImageView *barView = [[UIImageView alloc] initWithImage:bar];
+        UIImageView *crownView = [[UIImageView alloc] initWithImage:crown];
+        UIImageView *cherryView = [[UIImageView alloc] initWithImage:cherry];
+        UIImageView *lemonView = [[UIImageView alloc] initWithImage:lemon];
+        UIImageView *appleView = [[UIImageView alloc] initWithImage:apple];
+        
+        NSArray *imageViewArray = [[NSArray alloc] initWithObjects:sevenView, barView, crownView, cherryView, lemonView, appleView, nil];
+        
+        NSString *fieldName = [[NSString alloc] initWithFormat:@"column%d", i];
+        [self setValue:imageViewArray forKey:fieldName];
+    }
+    srandom(time(NULL));
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)spin {
+    BOOL win = NO;
+    int numInRow = 1;
+    int lastVal = -1;
+    for (int i=0; i<5; i++) {
+        int newValue = random() % [self.column1 count];
+        if (newValue == lastVal)
+            numInRow++;
+        else
+            numInRow = 1;
+        
+        lastVal = newValue;
+        [picker selectRow:newValue inComponent:i animated:YES];
+        [picker reloadComponent:i];
+        if (numInRow >= 3)
+            win = YES;
+    }
+    if (win)
+        winLabel.text = @"WIN!";
+    else
+        winLabel.text=@"";
+}
+
+#pragma mark -
+#pragma mark Picker Data Source Methods
+- (NSInteger)numberofComponentsInPickerView:(UIPickerView *)pickerView {
+    return 5;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return [self.column1 count];
+}
+
+#pragma mark Picker Delegate Methods
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
+    NSString *arrayName = [[NSString alloc] initWithFormat:@"column%d", component+1];
+    NSArray *array = [self valueForKey:arrayName];
+    return [array objectAtIndex:row];
 }
 
 @end
