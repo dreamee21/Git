fb8dd9b530efd0452d03b21da3230942368943d3
dreamee21 <itworks21@naver.com>
2013-08-03 21:16:57 +0900

    CollectionView and Scroll

--- CollectionView1/CollectionView1/ViewController.m
+++ CollectionView1/CollectionView1/ViewController.m
@@ -16,8 +16,38 @@
 @implementation ViewController
 @synthesize myWebView;
 @synthesize dataList;
+@synthesize scrollView1;
+@synthesize pageControl;
 
 int phoneType = 0;
+
+const CGFloat kScrollObjHeight	= 20.0;
+const CGFloat kScrollObjWidth	= 100.0;
+const NSUInteger kNumImages		= 5;
+
+
+- (void)layoutScrollImages
+{
+	UIImageView *view = nil;
+	NSArray *subviews = [scrollView1 subviews];
+    
+	// reposition all image subviews in a horizontal serial fashion
+	CGFloat curXLoc = 0;
+	for (view in subviews)
+	{
+		if ([view isKindOfClass:[UIImageView class]] && view.tag > 0)
+		{
+			CGRect frame = view.frame;
+			frame.origin = CGPointMake(curXLoc, 0);
+			view.frame = frame;
+			
+			curXLoc += (kScrollObjWidth);
+		}
+	}
+	
+	// set the content size so it can be scrollable
+	[scrollView1 setContentSize:CGSizeMake((kNumImages * kScrollObjWidth), [scrollView1 bounds].size.height)];
+}
 
 
 - (void)viewDidLoad
@@ -37,6 +67,40 @@ int phoneType = 0;
 	
     //Update products to selected phonetype
 	[self updateData];
+    
+	self.view.backgroundColor = [UIColor viewFlipsideBackgroundColor];
+    
+	// 1. setup the scrollview for multiple images and add it to the view controller
+	//
+	// note: the following can be done in Interface Builder, but we show this in code for clarity
+	[scrollView1 setBackgroundColor:[UIColor blackColor]];
+	[scrollView1 setCanCancelContentTouches:NO];
+	scrollView1.indicatorStyle = UIScrollViewIndicatorStyleWhite;
+	scrollView1.clipsToBounds = YES;		// default is NO, we want to restrict drawing within our scrollview
+	scrollView1.scrollEnabled = YES;
+	
+	// pagingEnabled property default is NO, if set the scroller will stop or snap at each photo
+	// if you want free-flowing scroll, don't set this property.
+	scrollView1.pagingEnabled = YES;
+	
+	// load all the images from our bundle and add them to the scroll view
+	NSUInteger i;
+	for (i = 1; i <= kNumImages; i++)
+	{
+		NSString *imageName = [NSString stringWithFormat:@"image%d.jpg", i];
+		UIImage *image = [UIImage imageNamed:imageName];
+		UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+		
+		// setup each frame to a default height and width, it will be properly placed when we call "updateScrollList"
+		CGRect rect = imageView.frame;
+		rect.size.height = kScrollObjHeight;
+		rect.size.width = kScrollObjWidth;
+		imageView.frame = rect;
+		imageView.tag = i;	// tag our images for later use when we place them in serial fashion
+		[scrollView1 addSubview:imageView];
+	}
+	[self layoutScrollImages];	// now place the photos in serial layout within the scrollview
+    
 }
 
 //Home Tab Change
@@ -59,7 +123,7 @@ int phoneType = 0;
             break;
     }
 
-	[self.collectionView reloadData];
+	[self.collectionView reloadData];    
 }
 
 - (void)didReceiveMemoryWarning
